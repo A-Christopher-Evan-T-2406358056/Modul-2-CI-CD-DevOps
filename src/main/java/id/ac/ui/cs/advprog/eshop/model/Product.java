86d8c6d05b2d9c3cb0d62980683bd1ca4fe0ee81
Evan <88143635+evan052006@users.noreply.github.com>
2026-02-04 11:13:18 +0700

    Added product UUID

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -2,10 +2,14 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import java.util.UUID;
 
 @Getter @Setter
 public class Product {
-    private String productId;
-    private String productName;
-    private int productQuantity;
+    public Product() {
+        this.productId = UUID.randomUUID().toString();
+    }
+    private final @Getter String productId;
+    private @Getter @Setter String productName;
+    private @Getter @Setter int productQuantity;
 }
